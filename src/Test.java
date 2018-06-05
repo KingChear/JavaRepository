74c7b946ff5e90fb9608799a96d59b6f99b3ddf9
Kingching <514840389@qq.com>
2018-06-05 16:50:46 +0800

    master

--- src/Test.java
+++ src/Test.java
@@ -6,10 +6,7 @@ public class Test {
     public static void main(String[] args) {
         int a[] = {1, 9, 6, 5, 11, 8, 1};
 
-        // init git
-        // this is content temp_branch1
-        // this is master and delete temp_branch1
-        // master add content
+
     }
 
 }
