1f6a870467197a36af9330cba09ef158d849b783
Kingching <514840389@qq.com>
2018-06-05 13:56:12 +0800

    clear test main

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,6 @@ public class Test {
 
     public static void main(String[] args) {
         int a[] = {1, 9, 6, 5, 11, 8, 1};
-        int binaryArray[] = {1, 2, 3, 5, 6, 77, 99};
-        System.out.println(selectAlgorithm.getInstance().orderSearch(a, 8));
 
     }
 
