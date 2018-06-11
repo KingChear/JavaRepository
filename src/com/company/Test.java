d86f4f48eafbff0e206a8af528937a586c4f6714
KingChear <514840389@qq.com>
2018-06-11 15:24:13 +0800

    java init

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         int a[] = {1, 9, 6, 5, 11, 8, 1};
-        
+
         System.out.println(selectAlgorithm.getInstance().binarySearch(a, 5));
 
 
