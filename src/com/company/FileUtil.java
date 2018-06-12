6a1dc142563264cadda8da341c38f5d45ea51ebb
KingChear <514840389@qq.com>
2018-06-12 17:40:10 +0800

    add FileUtil content

--- src/com/company/FileUtil.java
+++ src/com/company/FileUtil.java
@@ -1,7 +1,9 @@
 package com.company;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class FileUtil {
 
@@ -11,6 +13,7 @@ public class FileUtil {
 
     }
 
+
     public static FileUtil getInstance() {
         if (instance == null) {
             instance = new FileUtil();
@@ -18,9 +21,12 @@ public class FileUtil {
         return instance;
     }
 
+    /**
+     * 写文件
+     */
     public void writeFile() {
         try {
-            FileWriter fileWriter = new FileWriter("E:/临时/temp.txt");
+            FileWriter fileWriter = new FileWriter("F:/临时/temp.txt");
             fileWriter.write("Mike Mike@163.com");
             fileWriter.write("John John@163.com");
             fileWriter.close();
@@ -29,12 +35,50 @@ public class FileUtil {
         }
     }
 
-    public void writeByLine() {
+    /**
+     * 读文件
+     * @param file
+     * @return
+     */
+    public String readFile(File file) {
 
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(System.lineSeparator() + line);
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
     }
 
-    public void readFile() {
-
+    /**
+     * 下载网上图片资源
+     */
+    public void downloadFile() {
+        int bytesum = 0;
+        int byteread = 0;
+        try {
+            URL url = new URL("http://pimg.163.com/sms/micheal/logo.gif");
+            URLConnection connection = url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            FileOutputStream fileOutputStream = new FileOutputStream("F:/临时/tempgif.gif");
+            byte[] buffer = new byte[1444];
+            int length = 0;
+            while ((byteread = inputStream.read(buffer)) != -1) {
+                bytesum += byteread;
+                System.out.println(bytesum);
+                fileOutputStream.write(buffer, 0, byteread);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,11 +1,10 @@
 package com.company;
 
+import java.io.File;
+
 public class Test {
 
     public static void main(String[] args) {
-        int a[] = {1, 9, 6, 5, 11, 8, 1};
-
-        System.out.println(SelectAlgorithm.getInstance().binarySearch(a, 5));
 
     }
 
